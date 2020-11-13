43342cb2bf129a67914e5be3e45c369018378230
Ayman Patel <patelayman6@gmail.com>
2020-11-13 22:13:12 +0530

    Groovy

---
+++ Code/groovy-native-image/README.md
@@ -0,0 +1,3 @@
+# TODO
+
+https://www.youtube.com/watch?v=BjO_vBzaB4c
---
+++ Code/groovy-native-image/github-jobs.groovy
@@ -0,0 +1,17 @@
+import groovy.json.JsonSlurper
+
+def jobsUrl = "https://jobs.github.com/positions.json?description=groovy"
+
+def json = new JsonSlurper().parse(jobsUrl.toURL()) as List<Map>
+
+
+json.each {
+
+    println ("\n" + "=" * 42 )
+    println ("Title" +  it.title)
+    println ("Location" + it.location)
+    println "Company ${it.company}"
+    println ("Created at" + it.created_at)
+    println ("URL" + it.url)
+    println ("=" * 42)
+}
